74eebc843a80fb054ea20867109bac9197eac52f
Andy <zhumeilun0103@gmail.com>
2024-09-30 23:39:45 -0700

    finished.

--- app/src/main/java/com/example/homework2zhengzhupart2/ItemFragment.kt
+++ app/src/main/java/com/example/homework2zhengzhupart2/ItemFragment.kt
@@ -8,12 +8,22 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.homework2zhengzhupart2.databinding.FragmentFirstBinding
+import com.example.homework2zhengzhupart2.databinding.FragmentSecondBinding
+import com.example.homework2zhengzhupart2.databinding.FragmentThirdBinding
 import com.example.homework2zhengzhupart2.placeholder.PlaceholderContent
 
 /**
  * A fragment representing a list of Items.
  */
 class ItemFragment : Fragment() {
+
+    private var _binding: FragmentThirdBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     private var columnCount = 1
 
@@ -41,7 +51,21 @@ class ItemFragment : Fragment() {
                 adapter = MyItemRecyclerViewAdapter(PlaceholderContent.ITEMS)
             }
         }
-        return view
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonThird.setOnClickListener {
+            findNavController().navigate(R.id.action_ThirdFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
